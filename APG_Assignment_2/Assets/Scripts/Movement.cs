71d773a5a4f016504ae659aab7890601b3901f4a
Holly Clibbon <vendorofdoom@users.noreply.github.com>
2022-04-17 19:22:51 +0100

    Toy around with some steering behaviour

---
+++ APG_Assignment_2/Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Tutorials:
+// https://natureofcode.com/book/chapter-6-autonomous-agents/
+
+public class Movement : MonoBehaviour
+{
+
+    public float maxSpeed;
+    public float maxForce;
+    public float stoppingDist;
+
+    public Rigidbody rb;
+    public Transform target;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        SteerTowardsTarget();
+    }
+
+    private void SteerTowardsTarget()
+    {
+        Vector3 heading = (target.position - transform.position).normalized;
+
+        float distToTarget = Vector3.Distance(transform.position, target.position);
+        if (distToTarget <= stoppingDist)
+        {
+            heading *= Mathf.Lerp(0, maxSpeed, Mathf.InverseLerp(0, stoppingDist, distToTarget));
+
+            Debug.Log(Mathf.Lerp(0, maxSpeed, Mathf.InverseLerp(0, stoppingDist, distToTarget)));
+        }
+        else
+        {
+            heading *= maxSpeed;
+        }
+
+        Vector3 steer = Vector3.ClampMagnitude((heading - rb.velocity), maxForce);
+        rb.AddForce(steer);
+    }
+
+    private void Update()
+    {
+        transform.LookAt(target, Vector3.up);
+    }
+}
