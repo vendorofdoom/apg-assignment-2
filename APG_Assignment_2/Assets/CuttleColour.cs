4599c67a4c65fa1fd5281ef84cd31992630d704d
Holly Clibbon <vendorofdoom@users.noreply.github.com>
2022-04-19 18:38:28 +0100

    Add colour camouflage to cuttlefish shader

--- APG_Assignment_2/Assets/CuttleColour.cs
+++ APG_Assignment_2/Assets/CuttleColour.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class CuttleColour : MonoBehaviour
 {
+
+    public Color BaseColour1;
+    public Color BaseColour2;
+    public Color EyeColour;
+
+    public RenderTexture CamoCamTexture;  // TODO: Should we try to do this in shader? Can we have finer control over the transition in the shader? 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        Shader.SetGlobalColor("_CuttleBase1", BaseColour1);
+        Shader.SetGlobalColor("_CuttleBase2", BaseColour2);
+
     }
 }
